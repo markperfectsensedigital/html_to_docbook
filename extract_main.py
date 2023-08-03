ad36750901beb827c3db827f503f1b1c480ed346
Mark Lautman <mlautman@perfectsensedigital.com>
2023-08-03 15:20:48 -0400

    Crawl sitemap files, output URLs.

--- extract_main.py
+++ extract_main.py
@@ -1,9 +1,42 @@
 #!/Users/mlautman/virtual_python_environment/bin/python3.9
 
 from bs4 import BeautifulSoup
+from scrapy.spiders import SitemapSpider
+from scrapy.crawler import CrawlerProcess
+import sys
+
+
+class MySpider(SitemapSpider):
+	name = "bsp_sitemap"
+	sitemap_urls = ['https://www.brightspot.com/documentation/sitemap.xml']
+	sitemap_rules = [('/documentation/', 'parse_documentation')]
+	sitemap_follow = ['/documentation/']
+
+
+	def parse_documentation(self, response):
+		if response.url not in url_list:
+			url_list.append(response.url)
+
+url_list = []
+
+c = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/5.0',
+	'REQUEST_FINGERPRINTER_IMPLEMENTATION': '2.7',
+	'format': 'csv',
+})
+c.crawl(MySpider)
+c.start()
+url_list.sort()
+
+url_file = open("urls.txt","w")
+for myurl in url_list:
+	url_file.write("{0}\n".format(myurl))
+url_file.close()
+
+sys.exit()
+
 
 temporary_xml = open("temporary.xml", "w")
-
 with open("gs.html") as fp:
 	soup = BeautifulSoup(fp, 'html.parser')
 	main = soup.find('main')
