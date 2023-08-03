b3ddad244482dc42e47ee34646501b7118ac0a2b
Mark Lautman <mlautman@perfectsensedigital.com>
2023-08-03 07:18:21 -0400

    file cleanup

--- runit.sh
+++ runit.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-./html_to_docbook.py
+
+
+
+./extract_main.py
 if [ $? != 0 ]; then
   echo "Could not run html_to_docbook.py";
   exit
 fi
+echo "Created temporary.xml..."
+saxon -xsl:html_docbook.xsl -s:temporary.xml -o:ugly.xml
+if [ $? != 0 ]; then
+  echo "Could not transform to ugly.xml";
+  exit
+fi
 echo "Created ugly.xml..."
-
 xmllint --schema docbook.xsd --noout ugly.xml
 if [ $? != 0 ]; then
   echo "ugly.xml does not validate";
   exit
 fi
 echo "Validated ugly.xml..."
-
 xmllint --format --output docbook.xml ugly.xml
 if [ $? != 0 ]; then
   echo "Could not format ugly.xml into docbook.xml";
