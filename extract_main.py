cfd9b52e49698bf838030f8a1ef88884135fa7d5
Mark Lautman <mlautman@perfectsensedigital.com>
2023-08-07 13:34:13 -0400

    Add optional processing for extracting XML from HTML.

--- extract_main.py
+++ extract_main.py
@@ -29,7 +29,8 @@ parser = argparse.ArgumentParser(
                     prog='Documentation Retriever',
                     description='Retrieves all HTML files in Brightspot\'s documentation sitemaps.',
                     )
-parser.add_argument('-c','--crawl',action='store_const',const=False, default=False)
+parser.add_argument('-c','--crawl',action='store_const',const=False, default=False, help="Crawls the sitemaps to generate a list of URLs in the documentation set.")
+parser.add_argument('-e','--extract',action='store_const',const=False, default=False, help="Extracts the <main> element from the raw HTML file into an XML file in the directory xml_extracts/.")
 args = parser.parse_args()
 print("Running with following options:")
 print("* Crawl sitemaps: {}".format(args.crawl))
@@ -71,46 +72,53 @@ else:
 		print("Exception occurred: {0}".format(type(e).__name__))
 		sys.exit()
 
-ignored_urls = ['https://www.brightspot.com/documentation/', 'https://www.brightspot.com/documentation/4-2-x-x', 'https://www.brightspot.com/documentation/4-5-x-x', 'https://www.brightspot.com/documentation/4-7-releases']
 
-headers= {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+xml_extract_path = 'xml_extracts'
 
+if args.extract == True:
+	ignored_urls = ['https://www.brightspot.com/documentation/', 'https://www.brightspot.com/documentation/4-2-x-x', 'https://www.brightspot.com/documentation/4-5-x-x', 'https://www.brightspot.com/documentation/4-7-releases']
 
-dirpath = 'xml_extracts'
-if os.path.exists(dirpath):
-	shutil.rmtree(dirpath, ignore_errors=True)
-os.mkdir(dirpath)
+	headers= {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
 
-for my_url in url_list:
-	if my_url in ignored_urls:
-		continue
-	if not (validators.url(my_url)):
-		print("The following url is invalid, skipping: {0}".format(my_url))
-		continue
-
-	response = requests.get(my_url)
-	if response.status_code >= RESPONSE_CODE_BAD_MIN:
-		print("The following URL gave a status code of {0}: {1}".format(response.status_code,my_url))
-		continue
-
-	parsed_url = urllib.parse.urlparse(my_url)
-	my_url_path = parsed_url.path
-	my_url_path = my_url_path[1:]
-	my_url_path = my_url_path.replace('/','-')
-	my_url_path = dirpath + '/' + my_url_path
 	
-	temporary_xml = open(my_url_path +".xml", "w")
-	soup = BeautifulSoup(response.text, 'html.parser')
-	main = soup.find('main')
-	main_str = str(main)
-	main_str = main_str.replace('\x0a','')
-	main_str = main_str.replace('<main','<main xmlns="http://www.w3.org/TR/html4/"')
-	temporary_xml.write(main_str)
-	temporary_xml.close()
+	if os.path.exists(xml_extract_path):
+		shutil.rmtree(xml_extract_path, ignore_errors=True)
+	os.mkdir(xml_extract_path)
 
-	sys.exit()
+	for my_url in url_list:
+		if my_url in ignored_urls:
+			continue
+		if not (validators.url(my_url)):
+			print("The following url is invalid, skipping: {0}".format(my_url))
+			continue
+		print("Processing URL {0}".format(my_url))
+		response = requests.get(my_url)
+		if response.status_code >= RESPONSE_CODE_BAD_MIN:
+			print("The following URL gave a status code of {0}: {1}".format(response.status_code,my_url))
+			continue
 
+		parsed_url = urllib.parse.urlparse(my_url)
+		my_url_path = parsed_url.path
+		my_url_path = my_url_path[1:]
+		my_url_path = my_url_path.replace('/','-')
+		my_url_path = xml_extract_path + '/' + my_url_path
+		
+		temporary_xml = open(my_url_path +".xml", "w")
+		try:
+			soup = BeautifulSoup(response.text, 'html.parser')
+			main = soup.find('main')
+		except Exception as e:
+			print("Exception occurred: {0} for URL {1}".format(type(e).__name__), my_url)
+			sys.exit()
+		main_str = str(main)
+		main_str = main_str.replace('\x0a','')
+		main_str = main_str.replace('<main','<main xmlns="http://www.w3.org/TR/html4/"')
+		temporary_xml.write(main_str)
+		temporary_xml.close()
+
+for xml_input_file in os.listdir(xml_extract_path):
+	print(xml_input_file)
 
 sys.exit()
 
